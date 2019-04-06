c2ef92e288c0165a267b4a82018fa7dfac881621
Alexander  Danilowicz <alexander.j.danilowicz.19@dartmouth.edu>
2019-04-06 17:12:37 -0400

    STANDARDIZED JSONS

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 from urllib.parse import urlparse
 
+
 def get_domain(URL):
     parsed_uri = urlparse(URL)
     result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
@@ -12,8 +13,9 @@ def recieve_link(URL):
     print(response) # TOOD - error handling
 
     soup = BeautifulSoup(response, from_encoding=response.info().get_param('charset'), features="lxml")
+    soupStringList = convert_soup_to_str_list(soup)
 
-    find_too_many_headers(soup)
+    find_too_many_h1s(soup, URL)
 
     # find_bad_colors()
 
@@ -23,17 +25,39 @@ def recieve_link(URL):
 
     # find_respsonsive()
 
-def find_too_many_headers(soup):
-    PRINT_NAME = "too many headers"
-    TYPE = "too-many-headers"
+def find_too_many_h1s(soup, URL):
+    TYPE = "too many header elements"
     SEVERITY = "warning"
-    my_print(PRINT_NAME)
+    create_print_json(TYPE)
+    h1TagsList = soup.find_all('h1')
 
-    # code to find ...
+    if len(h1TagsList) > 1:
+        meta = h1TagsList
+        text = "You have " + str(len(h1TagsList)) + " h1 elements on " + str(URL)
+        create_error_json(TYPE, SEVERITY, URL, text=text, meta=meta)
+    create_success_json(TYPE)
 
-# printName example: "small text", "too many headers"
-def my_print(printName):
-    print("Looking for " + str(printName) + "... ")
+# severity types: warning, error
+def create_error_json(type, severity, URL, lineNumber=-1, text="", meta=""):
+    json = {"type": type, "severity": severity, "URL": URL, "lineNumber": lineNumber, "text": text, "meta": meta}
+    print(json)
+    return json
+
+# severity types: info
+def create_print_json(TYPE):
+    json = {"severity": "info", "text": ("Running analysis of " + str(TYPE) + "... ")}
+    print(json)
+    return json
+
+# severity type: success
+def create_success_json(TYPE):
+    json = {"severity": "success", "text": "Success, " + (str(TYPE)) + " test passed!"}
+    print(json)
+    return json
+
+def convert_soup_to_str_list(soup):
+    soupStringList = (str(soup).split("\n"))
+    return soupStringList
 
 URL = "http://www.alexanderdanilowicz.com/"
 recieve_link(URL)
